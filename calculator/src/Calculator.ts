05c2b43a90136172dfb24ed88eb415b6851e2c76
abhisheksr01 <abhisheksingh2691@gmail.com>
2022-04-12 18:48:01 +0100

    chore(#2): check circleci workflow trigger for calculator

--- calculator/src/Calculator.ts
+++ calculator/src/Calculator.ts
@@ -1,4 +1,5 @@
-function sum (num1:number, num2:number){
+function sum(num1: number, num2: number) {
+    console.log("The two numbers to add are num1: " + num1 + " and num2: " + num2);
     return num1 + num2;
 }
-console.log(sum(8,4))
+console.log(sum(8, 4))
