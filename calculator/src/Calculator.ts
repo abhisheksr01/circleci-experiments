90b97f166320c661b603f31611eadee64cf2cfff
abhisheksr01 <abhisheksingh2691@gmail.com>
2022-04-12 19:19:52 +0100

    feat(#2): add calculator exec job in circleci

--- calculator/src/Calculator.ts
+++ calculator/src/Calculator.ts
@@ -1,5 +1,5 @@
 function sum(num1: number, num2: number) {
-    console.log("The two numbers to add are num1: " + num1 + " and num2: " + num2);
+    console.log("Performing addition on num1: " + num1 + " and num2: " + num2);
     return num1 + num2;
 }
 console.log(sum(8, 4))
