71154427d88225bd8527bdc1be6c7770943d4d13
abhisheksr01 <abhisheksingh2691@gmail.com>
2022-04-13 15:21:14 +0100

    fix(#3): test slack notification with event always

--- calculator/src/Calculator.ts
+++ calculator/src/Calculator.ts
@@ -1,4 +1,5 @@
 function sum(num1: number, num2: number) {
+    console.log('I am inside Calculator App');
     console.log("Performing addition on num1: " + num1 + " and num2: " + num2);
     return num1 + num2;
 }
